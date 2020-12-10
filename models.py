943e1ca1ef2f6506e29a49908a32cc4b0ad861c1
helen2969 <>
2020-12-09 19:53:12 -0500

    test

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ if 'ON_HEROKU' in os.environ: # later we will manually add this env var
                                                      # when you provision the
                                                      # Heroku Postgres Add-on
 else:
-  DATABASE = SqliteDatabase('dogs.sqlite')
+  DATABASE = SqliteDatabase('walks.sqlite')
 
 
 class User(UserMixin, Model):
